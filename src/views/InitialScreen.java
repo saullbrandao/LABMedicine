b9a2bb1797556cf56b90949e3ed7bee44a964c87
Saull Brandão <saull@outlook.com>
2023-02-28 14:18:26 -0300

    fix: scanner infinity loop

--- src/views/InitialScreen.java
+++ src/views/InitialScreen.java
@@ -6,26 +6,28 @@ import java.util.Scanner;
 
 public class InitialScreen extends Screen {
     int selectedOption;
-    Scanner scanner = new Scanner(System.in);
 
     @Override
     public void show() {
-        try {
-            System.out.println("Bem vindo ao sistema LABMedicine");
-            System.out.println("Selecione uma das opções abaixo: ");
-            System.out.println("1. Cadastro de Paciente");
-            System.out.println("2. Cadastro de Enfermeiro");
-            System.out.println("3. Cadastro de Médico");
-            System.out.println("4. Realização de Atendimento Médico");
-            System.out.println("5. Atualização do Status de Atendimento do Paciente");
-            System.out.println("6. Relatórios");
-            System.out.println("Número selecionado: ");
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
 
-            selectedOption = scanner.nextInt();
-            navigate(selectedOption);
-        } catch (Exception e) {
-            System.out.println("Opção inválida. Tente novamente.");
-            show();
+            try {
+                System.out.println("Bem vindo ao sistema LABMedicine");
+                System.out.println("Selecione uma das opções abaixo: ");
+                System.out.println("1. Cadastro de Paciente");
+                System.out.println("2. Cadastro de Enfermeiro");
+                System.out.println("3. Cadastro de Médico");
+                System.out.println("4. Realização de Atendimento Médico");
+                System.out.println("5. Atualização do Status de Atendimento do Paciente");
+                System.out.println("6. Relatórios");
+                System.out.println("Número selecionado: ");
+
+                selectedOption = scanner.nextInt();
+                navigate(selectedOption);
+            } catch (Exception e) {
+                System.out.println("Opção inválida. Tente novamente.");
+            }
         }
     }
 
@@ -38,10 +40,8 @@ public class InitialScreen extends Screen {
             case 4 -> System.out.println("Realização de Atendimento Médico");
             case 5 -> System.out.println("Atualização do Status de Atendimento do Paciente");
             case 6 -> System.out.println("Relatórios");
-            default -> {
-                System.out.println("Opção inválida. Tente novamente.");
-                show();
-            }
+            default -> System.out.println("Opção inválida. Tente novamente.");
+
         }
     }
 
